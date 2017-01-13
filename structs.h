f2617a85dfbb44544428b9edac22675e09f28ac0
CatalinAnton <catalinanton505@gmail.com>
2017-01-13 20:21:17 +0200

    Add files via upload
    changed the colour
    fixed showing used cards

---
+++ structs.h
@@ -0,0 +1,23 @@
+#pragma once
+using namespace std;
+struct jucator
+{
+	int bani;
+	string name;
+};
+struct node
+{
+	int money;
+	string name;
+	node *next;
+};
+
+struct btree
+{
+	int card;
+	int frequency;
+	btree *left;
+	btree *right;
+
+};
+
